ba5fde78e69a56d2347557c2691768be446854b9
damienbod <damien_bod@hotmail.com>
2017-05-05 11:22:51 +0200

    configuring to app security

--- IdentityServerWithIdentitySQLite/Config.cs
+++ IdentityServerWithIdentitySQLite/Config.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -56,25 +57,20 @@ namespace QuickstartIdentityServer
                     //AccessTokenLifetime = 600, // 10 minutes, default 60 minutes
                     AllowedGrantTypes = GrantTypes.Hybrid,
                     AllowAccessTokensViaBrowser = true,
-                    RedirectUris = new List<string>
-                    {
-                        "https://localhost:44341"
-
-                    },
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "https://localhost:44341/"
-                    },
+                    RedirectUris = { "https://localhost:44341/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:44341/signout-callback-oidc" },
                     AllowedCorsOrigins = new List<string>
                     {
                         "https://localhost:44341/"
                     },
                     AllowedScopes = new List<string>
                     {
-                        "openid",
-                        "profile",
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
                         "thingsscope",
                         "role"
+
                     }
                 }
             };
