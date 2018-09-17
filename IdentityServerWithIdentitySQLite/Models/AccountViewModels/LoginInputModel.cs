b28553de774bae06f12de09d035b7c664eef023a
damienbod <damien_bod@hotmail.com>
2018-09-17 07:18:12 +0200

    updating STS project

--- IdentityServerWithIdentitySQLite/Models/AccountViewModels/LoginInputModel.cs
+++ IdentityServerWithIdentitySQLite/Models/AccountViewModels/LoginInputModel.cs
@@ -1,17 +1,16 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+﻿using System.ComponentModel.DataAnnotations;
 
-
-using System.ComponentModel.DataAnnotations;
-
-namespace IdentityServer4.Quickstart.UI.Models
+namespace StsServerIdentity.Models
 {
     public class LoginInputModel
     {
-        [Required]
+        [Required(ErrorMessage = "EMAIL_REQUIRED")]
+        [EmailAddress(ErrorMessage = "EMAIL_INVALID")]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "PASSWORD_REQUIRED")]
         public string Password { get; set; }
+
         public bool RememberLogin { get; set; }
         public string ReturnUrl { get; set; }
     }
