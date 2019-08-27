acb3ba47578dd8b792b24a79c260e868f84c522a
damienbod <damien_bod@hotmail.com>
2019-08-27 09:26:05 +0200

    Adding a logout

---
+++ AspNetCoreMvcAngular/Controllers/LogoutController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace AspNetCoreMvcAngular.Controller
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class LogoutController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        [HttpPost]
+        [Route("")]
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("OpenIdConnect");
+
+            return Ok();
+            //return new SignOutResult(new[] { "Cookies", "OpenIdConnect" });
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Add([FromBody] Thing thing)
+        //{
+        //    if (thing == null)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    Thing newThing = _thingsRepository.Add(thing);
+
+        //    return CreatedAtRoute("GetSingleThing", new { id = newThing.Id }, newThing);
+        //}
+    }
+}
