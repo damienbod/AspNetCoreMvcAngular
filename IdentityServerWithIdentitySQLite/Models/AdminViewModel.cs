53664633dc967e7d26719b8042ba09b4e860fa67
damienbod <damien_bod@hotmail.com>
2020-03-03 18:29:02 +0100

    updating STS

--- IdentityServerWithIdentitySQLite/Models/AdminViewModel.cs
+++ IdentityServerWithIdentitySQLite/Models/AdminViewModel.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace StsServerIdentity.Models
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
+    public class AdminViewModel
     {
+        [Required]
+        public string Email { get; set; }
         public bool IsAdmin { get; set; }
         public string DataEventRecordsRole { get; set; }
         public string SecuredFilesRole { get; set; }
